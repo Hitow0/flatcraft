b0dd677f81c8afa11da0d7f84d2f7f66d4e7df7d
Leo Denis <leo_denis@ens.univ-artois.fr>
2023-10-04 15:38:37 +0200

    Ajout de la fonction dig

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/Cellule.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/Cellule.java
@@ -1,6 +1,7 @@
 package fr.univartois.butinfo.r304.flatcraft.model.map;
 
 import fr.univartois.butinfo.r304.flatcraft.model.IMovable;
+import fr.univartois.butinfo.r304.flatcraft.model.movables.player.Player;
 import fr.univartois.butinfo.r304.flatcraft.model.resources.Resource;
 import fr.univartois.butinfo.r304.flatcraft.view.Sprite;
 
@@ -30,6 +31,14 @@ public class Cellule extends AbstractCell {
 
     @Override
     public boolean dig(IMovable player) {
+        if (this.getResource()!=null) {
+            if (this.getResource().getHardness()!=0) {
+                this.getResource().dig();
+            } else {
+                (Player)player.addObjet(this.getResource().digBlock());
+                return true;
+            }
+        }
         return false;
     }
 }
