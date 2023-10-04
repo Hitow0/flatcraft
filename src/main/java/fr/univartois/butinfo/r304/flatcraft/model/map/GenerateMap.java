0c2c7ac5705dec28fe1aa2c3175500d19c176c7c
Nicolas Nourry <nicolasnourry61@gmail.com>
2023-10-04 15:17:04 +0200

    update methode genMap de la classe GenerateMap

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/GenerateMap.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/GenerateMap.java
@@ -4,7 +4,18 @@ import fr.univartois.butinfo.r304.flatcraft.model.CellFactory;
 
 public class GenerateMap {
 
-    public void genMap(int height, int width, CellFactory cell){
+    public SimpleGameMap genMap(int height, int width, CellFactory cell){
         SimpleGameMap map = new SimpleGameMap(height, width, height / 2);
+        for(int i=0;i<height;i++){
+            for(int j=0;j<width;j++){
+                if(i>map.getSoilHeight())
+                    map.setAt(i,j,cell.createSky());
+                if(i==map.getSoilHeight())
+                    map.setAt(i,j, cell.createSoilSurface());
+                if(i< map.getSoilHeight())
+                    map.setAt(i,j,cell.createSubSoil());
+            }
+        }
+        return map;
     }
 }
