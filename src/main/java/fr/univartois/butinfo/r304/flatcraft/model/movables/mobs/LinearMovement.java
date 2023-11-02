e8d31ea5ec3ff6dd2ddb08253cf78dc24d278f3c
Matheo Dupuis <matheodupuis1@gmail.com>
2023-11-02 15:18:51 +0100

    :construction: Ajout de la classe LinearMovement

---
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/movables/mobs/LinearMovement.java
@@ -0,0 +1,10 @@
+package fr.univartois.butinfo.r304.flatcraft.model.movables.mobs;
+
+public class LinearMovement implements IMobStrategy{
+
+
+    @Override
+    public void mobMovement() {
+
+    }
+}
