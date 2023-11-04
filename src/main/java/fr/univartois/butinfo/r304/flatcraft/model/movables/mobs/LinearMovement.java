7f39dd0618de7f3f4e083675ddd0d0eed532cd1c
Matheo Dupuis <matheodupuis1@gmail.com>
2023-11-04 17:38:27 +0100

    :construction: Implémentation déplacement linéaire

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/movables/mobs/LinearMovement.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/movables/mobs/LinearMovement.java
@@ -2,9 +2,13 @@ package fr.univartois.butinfo.r304.flatcraft.model.movables.mobs;
 
 public class LinearMovement implements IMobStrategy{
 
+    Mob mob;
 
     @Override
-    public void mobMovement() {
+    public double mobMovement(double current, double speed, long delta, Mob mob) {
+        this.mob = mob;
+        this.mob.setHorizontalSpeed(-3*16);
+        return current + (delta * speed) / 1000;
 
     }
 }
