c2aae5f1e669a92302cba1746efd521b7a9ac374
nicolas <nicolasnourry61@gmail.com>
2023-11-04 14:37:15 +0100

    T2 fini

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/decorator/DecoSlagHeap.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/decorator/DecoSlagHeap.java
@@ -3,21 +3,21 @@ package fr.univartois.butinfo.r304.flatcraft.model.map.decorator;
 import fr.univartois.butinfo.r304.flatcraft.model.CellFactory;
 import fr.univartois.butinfo.r304.flatcraft.model.map.createMap.IGenMapStrat;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class DecoSlagHeap extends DecoratorMap{
-    private int MAX_HEIGHT=8;
 
-    private int height;
-    private int posX;
+    private final int height;
+    private final int posX;
 
     public DecoSlagHeap(IGenMapStrat genMapStrat) {
         super(genMapStrat);
         Random ran=new Random();
-        height=ran.nextInt(1,MAX_HEIGHT);
+        int MAX_HEIGHT = 8;
+        height=ran.nextInt(1, MAX_HEIGHT);
         posX=ran.nextInt(1,getMap().getWidth()-1);
-        posX=2;
-        height=4;
+
     }
 
     public void genSlagHeap(CellFactory cell){
@@ -33,7 +33,7 @@ public class DecoSlagHeap extends DecoratorMap{
                     for(int k=0;k<this.height;k++){
                         if(slagHeapWhith==1) {
                             getMap().setAt(i, j, cell.createSoilSurface());
-                            while(getMap().getAt(i,j).getResource().getName()=="water"){
+                            while(Objects.equals(getMap().getAt(i, j).getResource().getName(), "water")){
                                 getMap().setAt(i, j, cell.createSoilSurface());
                             }
                         }
@@ -43,37 +43,38 @@ public class DecoSlagHeap extends DecoratorMap{
                                 getMap().setAt(getMap().getSoilHeight(), posX,cell.createSubSoil());
 
                             }
-                            if(i-slagHeapWhith>0){
-                                for(int l=0;l<slagHeapWhith;l++){
-                                    if(posX-l>=0  && getMap().getAt(getMap().getSoilHeight()-posI-1, posX-l).getResource()==null) {
+                            System.out.println(j+","+slagHeapWhith);
+
+                            for(int l=0;l<slagHeapWhith;l++){
+                                if (posX-l >= 0) {
+                                    if (getMap().getAt(getMap().getSoilHeight() - posI - 1, posX - l).getResource() == null) {
                                         getMap().setAt(getMap().getSoilHeight() - posI, posX - l, cell.createSoilSurface());
-                                        while(getMap().getAt(getMap().getSoilHeight() - posI, posX - l).getResource().getName()=="water"){
+                                        while (Objects.equals(getMap().getAt(getMap().getSoilHeight() - posI, posX - l).getResource().getName(), "water")) {
                                             getMap().setAt(getMap().getSoilHeight() - posI, posX - l, cell.createSoilSurface());
                                         }
-                                    }else
+                                    } else
                                         getMap().setAt(getMap().getSoilHeight() - posI, posX - l, cell.createSubSoil());
-                                    if(posI==1){
-                                        getMap().setAt(getMap().getSoilHeight(), posX-l,cell.createSubSoil());
-
+                                    if (posI == 1) {
+                                        getMap().setAt(getMap().getSoilHeight(), posX - l, cell.createSubSoil());
                                     }
                                 }
                             }
-                            if(i+slagHeapWhith<width){
-                                for(int l=1;l<slagHeapWhith;l++){
-                                    if(getMap().getAt(getMap().getSoilHeight()-posI-1, posX+l).getResource()==null){
-                                        getMap().setAt(getMap().getSoilHeight()-posI, posX+l,cell.createSoilSurface());
-                                        while (getMap().getAt(getMap().getSoilHeight()-posI, posX+l).getResource().getName()=="water"){
-                                            getMap().setAt(getMap().getSoilHeight()-posI, posX+l,cell.createSoilSurface());
+
+
+                            for(int l=1;l<slagHeapWhith;l++){
+                                if(posX+l<width) {
+                                    if (getMap().getAt(getMap().getSoilHeight() - posI - 1, posX + l).getResource() == null) {
+                                        getMap().setAt(getMap().getSoilHeight() - posI, posX + l, cell.createSoilSurface());
+                                        while (Objects.equals(getMap().getAt(getMap().getSoilHeight() - posI, posX + l).getResource().getName(), "water")) {
+                                            getMap().setAt(getMap().getSoilHeight() - posI, posX + l, cell.createSoilSurface());
                                         }
-                                    }else
-                                        getMap().setAt(getMap().getSoilHeight()-posI, posX+l,cell.createSubSoil());
-                                    if(posI==1){
-                                        getMap().setAt(getMap().getSoilHeight(), posX+l,cell.createSubSoil());
+                                    } else
+                                        getMap().setAt(getMap().getSoilHeight() - posI, posX + l, cell.createSubSoil());
+                                    if (posI == 1) {
+                                        getMap().setAt(getMap().getSoilHeight(), posX + l, cell.createSubSoil());
 
                                     }
                                 }
-
-
                             }
 
                         }
