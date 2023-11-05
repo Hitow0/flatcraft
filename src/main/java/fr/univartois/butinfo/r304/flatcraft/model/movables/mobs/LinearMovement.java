b611bb52ecfca9728b763e3331133a760a1d7c50
Matheo Dupuis <matheodupuis1@gmail.com>
2023-11-05 17:10:20 +0100

    :hammer: Enlever l'attribut mob de LinearMovement

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/movables/mobs/LinearMovement.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/movables/mobs/LinearMovement.java
@@ -2,19 +2,18 @@ package fr.univartois.butinfo.r304.flatcraft.model.movables.mobs;
 
 public class LinearMovement implements IMobStrategy{
 
-    Mob mob;
+
 
     @Override
     public double mobMovement(double current, double speed, long delta, Mob mob, int limitMin, int limitMax) {
-        this.mob = mob;
-        this.mob.setHorizontalSpeed(-3*16);
+        mob.setHorizontalSpeed(-3*16);
         double newPos = current + (delta * speed) / 1000;
         if (newPos > limitMax + 32){
-            this.mob.setHorizontalSpeed(0);
+            mob.setHorizontalSpeed(0);
             return limitMax + 32;
         }
         else if (newPos < limitMin - 32){
-            this.mob.setHorizontalSpeed(0);
+            mob.setHorizontalSpeed(0);
             return limitMin - 32;
         }
         return newPos;
---
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/movables/mobs/randomMovement.java
@@ -0,0 +1,2 @@
+package fr.univartois.butinfo.r304.flatcraft.model.movables.mobs;public class randomMovement {
+}
