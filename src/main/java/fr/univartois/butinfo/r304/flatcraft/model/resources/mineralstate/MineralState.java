f1539f0dd636e8f74708f044700ebe79c6202c44
Leo Denis <leo_denis@ens.univ-artois.fr>
2023-11-22 15:02:29 +0100

    :sparkles: Ajout de l or

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/resources/mineralstate/MineralState.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/resources/mineralstate/MineralState.java
@@ -9,8 +9,10 @@ public class MineralState implements IStateResource {
 
     @Override
     public IStateResource changeState(Resource resource) {
-            resource.setName(resource.getName().substring(8)+"_lump");
-            resource.setSprite(SpriteStore.getInstance().getSprite(resource.getName()));
+        resource.setName(resource.getName().substring(8));
+        if ("coal".equals(resource.getName()) || "iron".equals(resource.getName()) || "gold".equals(resource.getName()))
+            resource.setName(resource.getName()+"_lump");
+        resource.setSprite(SpriteStore.getInstance().getSprite(resource.getName()));
         return new FinalState();
     }
 }
